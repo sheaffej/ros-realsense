5abd2cce25ac91328a3741816855e2fadc74abb3
sheaffej <sheaffej@gmail.com>
2020-02-01 16:37:34 -0800

    add docker log cmd to laserscan.sh

--- run/laserscan.sh
+++ run/laserscan.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 DOCKER_IMAGE="sheaffej/ros-realsense"
+CONTAINER_NAME="laserscan"
 
 [ -z "$ROS_MASTER_URI" ] && echo "Please set ROS_MASTER_URI env" && exit 1
 
 docker run -d --rm \
---name laserscan \
+--name ${CONTAINER_NAME} \
 --privileged \
 --net host \
 --env DISPLAY \
 --env ROS_MASTER_URI \
 ${DOCKER_IMAGE} roslaunch realsense2_camera laserscan.launch
 
+docker logs -f ${CONTAINER_NAME}
